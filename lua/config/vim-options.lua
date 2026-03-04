44288d017759068f4489ef9c36787c3f5d23945b
Mouad Benali <mouadbnl.2k01@gmail.com>
2026-03-04 21:48:41 +0000

    feat: added move line and selection

--- lua/config/vim-options.lua
+++ lua/config/vim-options.lua
@@ -15,10 +15,24 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 
 vim.diagnostic.config({
-  virtual_text = true,  -- Shows diagnostics inline
-  signs = true,         -- Gutter signs (e.g., ❌, ⚠️)
-  underline = true,     -- Underline errors
-  update_in_insert = false, -- Don't update while typing
-  severity_sort = true,     -- Sort diagnostics by severity
+	virtual_text = true,  -- Shows diagnostics inline
+	signs = true,         -- Gutter signs (e.g., ❌, ⚠️)
+	underline = true,     -- Underline errors
+	update_in_insert = false, -- Don't update while typing
+	severity_sort = true,     -- Sort diagnostics by severity
 })
 vim.opt.signcolumn = "yes"
+
+
+
+-- Move current line up/down in Normal mode
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+
+-- Move selected lines in Visual mode
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+-- Move line in Insert mode (returns to insert at same spot)
+vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down" })
+vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up" })
